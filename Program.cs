89b466ce09a46556dbdd3772b84de303828d120a
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-09-15 17:48:26 +0200

    cleaned up duplication

--- Program.cs
+++ Program.cs
@@ -11,15 +11,14 @@ for (int i = 0; i < len; i++)
 Console.WriteLine($"First Element: {favoriteFood[0]}");
 Console.WriteLine($"Last Element: {favoriteFood[len - 1]}");
 
+int middleIdx = len / 2;
+
 if (len % 2 == 0)
 {
-    int firstMiddleIdx = len / 2 - 1;
-    int secondMiddleIdx = len / 2;
-    Console.WriteLine($"Bonus - Middle Element #1: { favoriteFood[firstMiddleIdx]}");
-    Console.WriteLine($"Bonus - Middle Element #2: {favoriteFood[secondMiddleIdx]}");
+    Console.WriteLine($"Bonus - Middle Element #1: {favoriteFood[middleIdx]}");
+    Console.WriteLine($"Bonus - Middle Element #2: {favoriteFood[middleIdx - 1]}");
 }
 else
 {
-    int middleIdx = len / 2;
-    Console.WriteLine($"Bonus - Middle Element: { favoriteFood[middleIdx]}");
+    Console.WriteLine($"Bonus - Middle Element: {favoriteFood[middleIdx]}");
 }
