43ec4e4e18b369f1cbf79fb6d0d6346c466d8678
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-09-15 17:19:44 +0200

    add logic to handle better even array lengths

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] favoriteFood = { "Carbonara", "Sushi", "Pizza", "Cannelloni", "Pasta al forno" };
+﻿string[] favoriteFood = { "Carbonara", "Sushi", "Pizza", "Cannelloni", "Pasta al forno", "A" };
 int len = favoriteFood.Length;
 
 Console.WriteLine($"Length: {len}");
@@ -11,5 +11,15 @@ for (int i = 0; i < len; i++)
 Console.WriteLine($"First Element: {favoriteFood[0]}");
 Console.WriteLine($"Last Element: {favoriteFood[len - 1]}");
 
-int middle = (len / 2) % 2 == 0 ? len / 2 : len / 2 - 1;
-Console.WriteLine($"Bonus - Middle Element: { favoriteFood[middle]}");
+if (len % 2 == 0)
+{
+    int firstMiddleIdx = len / 2 - 1;
+    int secondMiddleIdx = len / 2;
+    Console.WriteLine($"Bonus - Middle Element #1: { favoriteFood[firstMiddleIdx]}");
+    Console.WriteLine($"Bonus - Middle Element #2: {favoriteFood[secondMiddleIdx]}");
+}
+else
+{
+    int middleIdx = len / 2;
+    Console.WriteLine($"Bonus - Middle Element: { favoriteFood[middleIdx]}");
+}
