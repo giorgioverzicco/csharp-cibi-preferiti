0e8844aaa7139a926c577b6d602811e0a3c79538
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-09-15 15:20:05 +0200

    End of exercise

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] favoriteFood = { "Carbonara", "Sushi", "Pizza", "Cannelloni", "Pasta al forno" };
+int len = favoriteFood.Length;
+
+Console.WriteLine($"Length: {len}");
+
+for (int i = 0; i < len; i++)
+{
+    Console.WriteLine($"{i}: {favoriteFood[i]}");
+}
+
+Console.WriteLine($"First Element: {favoriteFood[0]}");
+Console.WriteLine($"Last Element: {favoriteFood[len - 1]}");
+
+int middle = (len / 2) % 2 == 0 ? len / 2 : len / 2 - 1;
+Console.WriteLine($"Bonus - Middle Element: { favoriteFood[middle]}");
